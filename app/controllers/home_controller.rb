036b191fa6b9aeaad95b4573faf79d3f0baeb69f
parkhee <gmldbd123@gmail.com>
2017-08-10 08:24:46 +0000

    hoooooo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
   def makeQuest_new
     @user = current_user
+    if @user.status == 1
+      redirect_to '/makeQuest_quest'
+    end
+   
     
     @quest1 = Quest.where(theme: '정').sample
     @quest2 = Quest.where(theme: '자기계발').sample
@@ -12,16 +16,18 @@ class HomeController < ApplicationController
   end
   
   def selectQuest   # makeQeust_user창에서 퀘스트를 발급 받으면 사용자와 퀘스트 간에의 관계를 묶어 주는 메소드입
-    @user = current_user
+    user = current_user
+    user.update(status: true)
     
-    @userquest = Userquest.create(user_id: params[:user_id], quest_id: params[:quest_id], status: "selected")
+    @userquest = Userquest.create(user_id: params[:user_id], quest_id: params[:quest_id])
     redirect_to '/makeQuest_quest'
   
   end
 
   def makeQuest_quest
     
-    @userquest = Userquest.find_by(status: "selected")
+    @user = current_user
+    @userquest = Userquest.find_by(user_id: @user.id)
     
   end
   
